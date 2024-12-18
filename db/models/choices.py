9527d02f9096a73ad963cb93729cd77639a88bc6
Ian Kabaka <iankabaka1@gmail.com>
2024-12-18 21:13:23 +0300

    creates a class choice and adds getters and setters

---
+++ db/models/choices.py
@@ -0,0 +1,24 @@
+from db_connection import get_db_connection
+class Choice:
+    def __init__(self,question_id,choice_text,is_correct):
+        self._choice_id = None
+        self._question_id = question_id
+        self._choice_text = choice_text
+        self._is_correct = is_correct
+    @property
+    def question_id(self):
+        return self._question_id
+    @question_id.setter
+    def question_id(self, question_id):
+        if not isinstance(question_id, int):
+            raise ValueError("the question id must be an integer")
+        self._question_id = question_id
+    @property
+    def choice_text(self):
+        return self._choice_text
+
+    @choice_text.setter
+    def choice_text(self, choice_text):
+        if not isinstance(choice_text, str):
+            raise ValueError("choice text must be a string")
+        self._choice_text = choice_text
