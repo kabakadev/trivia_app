3244c672ca299aded165197b72fd968b0c267c90
Ian Kabaka <iankabaka1@gmail.com>
2024-12-19 03:37:28 +0300

    added more functionality for checking on user

--- main.py
+++ main.py
@@ -6,13 +6,7 @@ from db.models.users import User
 def check_user_priviledges():
     if User.get_all_users() is None:
         print("No users found here, we need atleast one admin account")
-        while True:
-            username = input("Enter a username for the admin:")
-            try:
-                User.get_user_by_username(username)
-                print("Username found, try a new one")
-            except:
-                break
+        username = input("Enter a username for the admin:") 
         while True:
             is_admin = input("This user has to be an amdin type: (yes)").lower()
             if is_admin in ["yes"]:
@@ -23,3 +17,18 @@ def check_user_priviledges():
         admin_user = User(username,is_truly_admin)
         admin_user.save()
         print(f"Admin User '{username}' has been created successfully")
+def main_menu():
+    while True:
+        username = input("Enter your username: ")
+        current_user = User.get_user_by_username(username)
+        if current_user is None:
+            print("Username not found, we will create a new user...")
+            is_admin_input = input("Is this user an admin? (yes/no): ").lower()
+            if is_admin_input in ["yes", "no"]:
+                is_admin = is_admin_input == "yes"
+
+            else:
+                print("Invalid input. Please enter 'yes' or 'no'.")
+            local_or_admin_user = User(username, is_admin)
+            local_or_admin_user.save()
+            print(f"User '{username}' created successfully!")
