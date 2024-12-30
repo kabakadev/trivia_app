91223a4d285d57807d61ef6fc439cc6190556e1a
Ian Kabaka <iankabaka1@gmail.com>
2024-12-30 13:33:42 +0300

    refactored to hndle menus for admin and regular using dictionaries and lambda functions

--- main.py
+++ main.py
@@ -83,3 +83,22 @@ def display_menu(user):
     
     adminOptions = ["Add new question", "Delete question"] + options
     return adminOptions if user.is_admin else options
+def handle_menu_choice(choice,user):
+    """Handle user menu choices"""
+    admin_actions = {
+        0:lambda:create_questions(user.user_id),
+        1:delete_questions
+    }
+    common_actions = {
+        0:view_all_questions,
+        1:lambda:play_trivia(user.user_id),
+        2:lambda:print(f"Logging out {user.username}"),
+        3:lambda:exit("exiting the trivia app")
+    }
+    if user.is_admin:
+        if choice in admin_actions:
+            admin_actions[choice]()
+        else:
+            common_actions[choice -2]()
+    else:
+        common_actions[choice]()
